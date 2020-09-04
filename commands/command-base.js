5d2d3598424ac255d1e4e036f5782668ca1c30b5
Kamatoro64 <chester91.jp@gmail.com>
2020-09-04 11:15:47 +0800

    Add code that converts commands and permissions to arrays in case a single string was supplied on commandOptions. Add validatePermissions function to check if permissions supplied are valid discord permissions

--- commands/command-base.js
+++ commands/command-base.js
@@ -1,5 +1,47 @@
 const { prefix } = require('../config.json')
 
+const validatePermissions = (permission) => {
+	const validPermissions = [
+		'ADD_REACTIONS',
+		'ADMINISTRATOR',
+		'ATTACH_FILES',
+		'BAN_MEMBERS',
+		'CHANGE_NICKNAME',
+		'CONNECT',
+		'CREATE_INSTANT_INVITE',
+		'DEAFEN_MEMBERS',
+		'EMBED_LINKS',
+		'KICK_MEMBERS',
+		'MANAGE_CHANNELS',
+		'MANAGE_EMOJIS',
+		'MANAGE_GUILD',
+		'MANAGE_MESSAGES',
+		'MANAGE_NICKNAMES',
+		'MANAGE_ROLES',
+		'MANAGE_WEBHOOKS',
+		'MENTION_EVERYONE',
+		'MOVE_MEMBERS',
+		'MUTE_MEMBERS',
+		'PRIORITY_SPEAKER',
+		'READ_MESSAGE_HISTORY',
+		'SEND_MESSAGES',
+		'SEND_TTS_MESSAGES',
+		'SPEAK',
+		'STREAM',
+		'USE_EXTERNAL_EMOJIS',
+		'USE_VAD',
+		'VIEW_AUDIT_LOG',
+		'VIEW_CHANNEL',
+		'VIEW_GUILD_INSIGHTS'
+	]
+
+	for (const permission of permissions) {
+		if (!validPermissions.includes(permission)) {
+			throw new Error(`Unknown permission node "${permission}"`)
+		}
+	}
+}
+
 module.exports = (client, commandOptions) => {
 	/* 
 	Destructuring the commandOptions and setting defaults
@@ -18,6 +60,23 @@ module.exports = (client, commandOptions) => {
 		requiredRoles = [],
 		callback
 	} = commandOptions
+
+	// Convert commands into an array if a single string was set for comamands (no aliases)
+	if (typeof commands === 'string') {
+		commands = [commands]
+	}
+
+
+	if (permissions.length) { // Check if exists, works for string or array
+		if (typeof permissions === 'string') {
+			permissions = [permissions]
+		}
+
+		// Check that each permission in the permissions array is a valid discord permission
+		validatePermissions(permissions)
+
+
+	}
 
 
 }
