760cd7df01ff04bab68cc555d94a30d142ce9af9
Kamatoro64 <chester91.jp@gmail.com>
2020-09-04 10:41:54 +0800

    Adding the commandOptions for the add command under add.js

---
+++ commands/add.js
@@ -0,0 +1,13 @@
+module.exports = {
+	commands: ['add', 'addition'], //string or array (aliases)
+	expectedArgs: '<num1> <num2>', // Usage
+	permissionError: 'You need admin permissions to run this command',
+	minArgs: 2,
+	maxArgs: 2,
+	callback: (message, arguments, text) => {
+		// example - !ticket <long text>
+
+	},
+	permissions: [],
+	requiredRoles: []
+}
