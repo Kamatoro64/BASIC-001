363341a4c0d96d9ec0db836a760dc918cb20b9d1
Kamatoro64 <chester91.jp@gmail.com>
2020-09-05 17:19:29 +0800

    Cleaned up index.js, removing old commands and commened tou welcome and messageCounter commands

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const fs = require('fs')
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require('./config.json') // Config is a JSON Object
-const command = require('./command')
 const mongo = require('./mongo')
-const welcome = require('./welcome')
 const messageCounter = require('./message-counter')
 
 client.on('ready', async () => {
@@ -34,10 +32,8 @@ client.on('ready', async () => {
 			} else if (file !== baseFile) {
 				const option = require(path.join(__dirname, dir, file))
 				commandBase(client, option)
-
 			}
 		}
-
 	}
 
 	readCommands('commands') // Directory called commands
@@ -46,46 +42,9 @@ client.on('ready', async () => {
 
 
 
-	messageCounter(client)
+	//messageCounter(client)
 
-	welcome(client)
-
-
-
-
-	// Note that the 3rd parameter to the command function is a callback function
-	// This is where the logic for a particular command is coded
-	command(client, 'ping', message => {
-		message.channel.send('pong')
-	})
-
-	command(client, 'servers', message => {
-		client.guilds.cache.forEach(guild =>
-			message.channel.send(`${guild.name} has a total of ${guild.memberCount} members`))
-
-		/* guild.name = server name, guild.memberCount = channel*/
-	})
-
-	command(client, ['cc', 'clearchannel'], message => {
-		if (message.member.hasPermission('ADMINISTRATOR')) {
-			message.channel.messages.fetch().then(results => {
-				message.channel.bulkDelete(results)
-			})
-		}
-	})
-
-	command(client, 'status', message => {
-		const content = message.content.replace('!status ', '')
-
-		client.user.setPresence({
-			activity: {
-				name: content,
-				type: 0
-			}
-		})
-	})
-
-
+	//welcome(client)
 })
 
 
