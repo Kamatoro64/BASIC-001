6ab725c241cddcc037a0098b392a66a6fce1fa34
Kamatoro64 <chester91.jp@gmail.com>
2020-09-07 01:52:00 +0800

    Implement proper callback to balance command. I.e. it calls the getCoins function and replies to the user the no. of coins for eiter the author or the tagged user

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -1,12 +1,18 @@
+const economy = require('../../economy')
+
 module.exports = {
 	commands: ['balance', 'bal'],
 	maxArgs: 1,
 	expectedArgs: "<Target user's @>",
-	callback: (message) => {
+	callback: async (message) => {
 		const target = message.mentions.users.first() || message.author // Defaults tp message.author 
 		const targetId = target.id
 
-		console.log('ID: ', targetId)
+		const guildId = message.guild.id //Check the guild (server) in which the message was sent
+		const userId = target.id
+
+		const coins = await economy.getCoins(guildId, userId) //Hence callback has to be asynchronous
+		message.reply(`That user has ${coins} coins`)
 	}
 
 }
