61c63ab10aa9c4e00a3964c1980f3098472ffff2
Kamatoro64 <chester91.jp@gmail.com>
2020-09-07 01:19:41 +0800

    Adding comment to balance.js

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -3,7 +3,7 @@ module.exports = {
 	maxArgs: 1,
 	expectedArgs: "<Target user's @>",
 	callback: (message) => {
-		const target = message.mentions.users.first() || message.author
+		const target = message.mentions.users.first() || message.author // Defaults tp message.author 
 		const targetId = target.id
 
 		console.log('ID: ', targetId)
